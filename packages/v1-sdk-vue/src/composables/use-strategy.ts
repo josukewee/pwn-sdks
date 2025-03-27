be9f0a66ea5b9685c19ebb1c015ad6791fd97368
Skas Merkushin <skas.merkushin@gmail.com>
2025-03-27 16:18:09 +0100

    fix(vue-sdk): stratgies build

--- packages/v1-sdk-vue/src/composables/use-strategy.ts
+++ packages/v1-sdk-vue/src/composables/use-strategy.ts
@@ -1,10 +1,11 @@
 import { getStrategy } from "@pwndao/v1-core";
+import type { Strategy } from "@pwndao/v1-core";
 import { useQuery } from "@tanstack/vue-query";
 
 export const useStrategy = (strategyId: string) => {
-	return useQuery({
+	return useQuery<Strategy, Error>({
 		queryKey: ["strategy", strategyId],
-		queryFn: ({ queryKey }) => getStrategy(queryKey[1]),
+		queryFn: ({ queryKey }) => getStrategy(queryKey[1] as string),
 		enabled: !!strategyId,
 	});
 };
