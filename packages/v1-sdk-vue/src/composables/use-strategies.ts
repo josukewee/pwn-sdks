31a94309a60fdbac5828eac43cf3b195707b0995
Skas Merkushin <skas.merkushin@gmail.com>
2025-03-28 07:43:51 +0100

    fix: vue packages ractivity and envs

--- packages/v1-sdk-vue/src/composables/use-strategies.ts
+++ packages/v1-sdk-vue/src/composables/use-strategies.ts
@@ -5,8 +5,10 @@ import { type MaybeRefOrGetter, computed, toValue } from "vue";
 
 export const useStrategies = (chainId: MaybeRefOrGetter<SupportedChain>) => {
 	const _chainId = computed(() => toValue(chainId));
+	const queryIsEnabled = computed(() => !!_chainId.value);
 	return useQuery({
 		queryKey: ["strategies", _chainId],
 		queryFn: ({ queryKey }) => getStrategies(queryKey[1] as SupportedChain),
+		enabled: queryIsEnabled,
 	});
 };
