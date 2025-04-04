198a93180fbd9d14582e3eb4fb21ec2a3fc2b071
tohrxyz <tomas.hrib@proton.me>
2025-04-04 19:47:22 +0200

    fix: use computed/usestate for contracts

--- packages/v1-sdk-react/src/hooks/use-elastic-chainlink-collateral-amount.ts
+++ packages/v1-sdk-react/src/hooks/use-elastic-chainlink-collateral-amount.ts
@@ -4,17 +4,18 @@ import { getCollateralAmount, IProposalSpecificParams } from "@pwndao/v1-core";
 import { ChainLinkProposalContract } from "@pwndao/v1-core";
 import { ElasticProposalContract } from "@pwndao/v1-core";
 import { ProposalType } from "@pwndao/v1-core";
+import { useState } from "react";
 
 
 export const useGetCollateralAmount = (params: IProposalSpecificParams) => {
     const config = useConfig()
-    let contract: ElasticProposalContract | ChainLinkProposalContract
-
-    if (params.type === ProposalType.Elastic) {
-        contract = new ElasticProposalContract(config)
-    } else {
-        contract = new ChainLinkProposalContract(config)
-    }
+    const [contract, _] = useState<ElasticProposalContract | ChainLinkProposalContract>(() => {
+        if (params.type === ProposalType.Elastic) {
+            return new ElasticProposalContract(config);
+        } else {
+            return new ChainLinkProposalContract(config);
+        }
+    });
 
     return useQuery({
         queryKey: ["get-proposal-collateral-amount", params],
