d496549032327343f546460827595458f30f2569
Skas Merkushin <skas.merkushin@gmail.com>
2025-04-10 03:42:46 +0200

    chore: export base proposals class

--- packages/v1-core/src/contracts/index.ts
+++ packages/v1-core/src/contracts/index.ts
@@ -2,3 +2,4 @@ export * from "./elastic-proposal-contract.js";
 export * from "./revoked-nonce-contract.js";
 export * from "./chain-link-proposal-contract.js";
 export * from "./simple-loan-contract.js";
+export * from "./base-proposal-contract.js";
